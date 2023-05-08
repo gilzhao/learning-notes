a3b4bd8cca543a0ab33aae0b7acd11c1a9ae84fb
gil.zhao <gil.zhao@qq.com>
2023-05-08 15:40:55 +0800

    add mini-rollup

---
+++ week03-mini-rollup/begin/index.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const acorn = require("acorn");
+const MagicString = require("magic-string");
+
+const code = fs.readFileSync("./source.js").toString();
+const ast = acorn.parse(code, {
+  sourceType: "module",
+  ecmaVersion: 7,
+});
+
+// console.log("ast", ast);
+
+// 遍历 => 查找变量声明
+const declarations = {};
+
+ast.body
+  .filter((v) => v.type === "VariableDeclaration")
+  .map((v) => {
+    console.log("声明", v.declarations[0].id.name);
+    declarations[v.declarations[0].id.name] = v;
+  });
+
+console.log(declarations);
+
+const statements = [];
+// 遍历 => 将声明放在调用前
+// a() =>   const a = () => 1; a()
+ast.body
+  .filter((v) => v.type !== "VariableDeclaration")
+  .map((node) => {
+    statements.push(declarations[node.expression.callee.name]);
+    statements.push(node);
+  });
+
+
+console.log("🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀");
+// 导出
+const m = new MagicString(code);
+statements.map((node) => {
+  console.log(m.snip(node.start, node.end).toString());
+});
+console.log("🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀");
---
+++ week03-mini-rollup/begin/source.js
@@ -0,0 +1,9 @@
+
+// Treeshaking
+const a = () => 1
+const b = () => 2 // 删除
+const c = () => 3 // 删除
+
+a()
+c()
+c()
